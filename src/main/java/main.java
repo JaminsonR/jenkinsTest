b057c8c5906da8abfb49cbcc842742971f4bd6eb
Jaminson Riascos <jami.r.m@hotmail.com>
2019-01-08 16:26:33 -0500

    gradle

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -4,8 +4,15 @@ import java.util.Scanner;
 
 public class main {
 
+	private static int a;
+
+	public static void main(String[] args) {
 	
-    System.out.println("Opcion no disponible");
- 
-	
+		
+		
+		System.out.println(" * * * Calculador * * *");
+		
+
+	}
+
 }
